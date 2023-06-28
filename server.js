8909911bb9d42df9f53cb65329b16261299fa3f9
Ankit Patil <22amtics124@gmail.com>
2023-06-28 18:50:30 +0530

    adding friend on home page

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 // Connecting to the database
-var conn = mysql.createConnection({
+var mainConn = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "chatting webapp",
 });
 
-conn.connect(function (err) {
+mainConn.connect(function (err) {
   if (err) throw err;
   console.log("Connected to the Database!");
 });
@@ -28,62 +28,82 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./tamplates/index.html"));
 });
 
-
-
 app.post("/home.pug", (req, res) => {
   console.log(req.body);
   if (req.body.formType == "login") {
-    const email = req.body.email;
+    const username = req.body.username;
     const password = req.body.password;
 
-    const query = `SELECT * FROM \`user information\` WHERE email = '${email}';`;
-    const username = conn.query(query, function (err, result) {
+    const query = `SELECT * FROM \`user information\` WHERE username = '${username}';`;
+    mainConn.query(query, function (err, result) {
       if (err) throw err;
       console.log(result[0].username);
       const username = result[0].username;
       res.redirect(`/home.pug?username=${username}`);
-      return;
     });
-
-    // const params = { "email": email, "password": password };
-    // res.render(
-    //   path.join(
-    //     __dirname,
-    //     `./tamplates/home.pug`
-    //   ),
-    //   params
-    // );
-
-    
-
-  }
-  else if (req.body.formType == "signup") {
+  } else if (req.body.formType == "signup") {
     let email = req.body.email;
     let password = req.body.password;
     let username = req.body.username;
 
-    const params = { "email": email, "password": password, "username": username };
+    const params = { email: email, password: password, username: username };
     res.render(path.join(__dirname, "./tamplates/home.pug"), params);
 
-    const query =
+    // SQL to add user in table
+    const query1 =
       "INSERT INTO `user information` (`id`, `email`, `username`, `password`) VALUES (?, ?, ?, ?);";
     const values = ["", email, username, password];
-      conn.query(query, values, function (err, result) {
-        if (err) throw err;
-        console.log("Table updated");
-      });
-    }
+    const id = mainConn.query(query1, values, function (err, result) {
+      if (err) throw err;
+      console.log("Table updated");
+    });
 
-  
+    // SQL to create new db for user
+    const query2 = `CREATE DATABASE BT_${username}`;
+    mainConn.query(query2, function (err, result) {
+      if (err) throw err;
+      console.log(`Created new database BT_${username}`);
+    });
+  } else if (req.body.formType == "addFriend") {
+    const username = req.body.username;
+    const f_username = req.body.username;
+
+    // connecting to user's database
+    var userConn = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: `bt_${username}`,
+    });
+    userConn.connect(function (err) {
+      if (err) throw err;
+      console.log(`Connected to the bt_${username} Database!`);
+    });
+
+    // Getting all details of friend
+    const query = `SELECT * FROM \`user information\` WHERE username = '${f_username}';`;
+    const f_details = mainConn.query(query, function (err, result) {
+      if (err) throw err;
+      console.log(result[0].username);
+      return;
+    });
+    console.log(f_details);
+
+    // const query = `INSERT INTO \`friends\` (\`f-id\`, \`f-email\`, \`f-username\`) VALUES ('1', 'abc@gmail.com', 'An');`;
+    // mainConn.query(query, function (err, result) {
+    //   if (err) throw err;
+    //   console.log(result[0].username);
+    //   const username = result[0].username;
+    //   res.redirect(`/home.pug?username=${username}`);
+    // });
   }
-);
+});
 
 app.get("/home.pug", (req, res) => {
-  const username = req.query.username
+  const username = req.query.username;
   console.log(`get ${username}`);
-  
-  res.render(path.join(__dirname, "./tamplates/home.pug"), { username });
 
+  res.render(path.join(__dirname, "./tamplates/home.pug"), { username });
 });
 
 app.listen(port, () => {
