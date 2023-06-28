ff1a7d297273eae0c60a44ba88140560cdc6fb1f
Ankit Patil <22amtics124@gmail.com>
2023-06-28 11:59:35 +0530

    completed user signin signup process in backend

--- server.js
+++ server.js
@@ -28,44 +28,62 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./tamplates/index.html"));
 });
 
-app.get("/signup.pug", (req, res) => {
-  res.render(path.join(__dirname, "./tamplates/signup.pug"));
-});
+
 
 app.post("/home.pug", (req, res) => {
+  console.log(req.body);
+  if (req.body.formType == "login") {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  if (req.body.formTitle === "login") {
-    const { formTitle, email, password } = req.body;
-    console.log(email)
+    const query = `SELECT * FROM \`user information\` WHERE email = '${email}';`;
+    const username = conn.query(query, function (err, result) {
+      if (err) throw err;
+      console.log(result[0].username);
+      const username = result[0].username;
+      res.redirect(`/home.pug?username=${username}`);
+      return;
+    });
 
-    // var sqquery =
-    //   "INSERT INTO `user information` (`password`, `id`, `email`, `username`) VALUES ('');";
-    // conn.query(sql, function (err, result) {
-    //   if (err) throw err;
-    //   console.log("Table created");
-    // });
+    // const params = { "email": email, "password": password };
+    // res.render(
+    //   path.join(
+    //     __dirname,
+    //     `./tamplates/home.pug`
+    //   ),
+    //   params
+    // );
+
+    
+
   }
-  // else if (req.body.formTitle === "signup") {
-  //   let email = req.body.email;
-  //   let password = req.body.password;
-  //   let username = req.body.username;
+  else if (req.body.formType == "signup") {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
 
-  //   var sql =
-  //     "INSERT INTO `user information` (`password`, `id`, `email`, `username`) VALUES ('" +
-  //     password +
-  //     "', '', '" +
-  //     email +
-  //     "', '" +
-  //     "testUser" +
-  //     "');";
-  //   conn.query(sql, function (err, result) {
-  //     if (err) throw err;
-  //     console.log("Table created");
-  //   });
-  // }
+    const params = { "email": email, "password": password, "username": username };
+    res.render(path.join(__dirname, "./tamplates/home.pug"), params);
 
-  const params = { email: email, password: password };
-  res.render(path.join(__dirname, "./tamplates/home.pug"), params);
+    const query =
+      "INSERT INTO `user information` (`id`, `email`, `username`, `password`) VALUES (?, ?, ?, ?);";
+    const values = ["", email, username, password];
+      conn.query(query, values, function (err, result) {
+        if (err) throw err;
+        console.log("Table updated");
+      });
+    }
+
+  
+  }
+);
+
+app.get("/home.pug", (req, res) => {
+  const username = req.query.username
+  console.log(`get ${username}`);
+  
+  res.render(path.join(__dirname, "./tamplates/home.pug"), { username });
+
 });
 
 app.listen(port, () => {
